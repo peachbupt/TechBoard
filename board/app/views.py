8f4566a57393c2ab1820a6ce2bd905b93286cdd4
jinxingh <jinxingh@vmware.com>
2017-01-25 09:45:00 +0800

    Add Board site index page

--- board/app/views.py
+++ board/app/views.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route('/', methods =['POST', 'GET'])
 @app.route('/index/', methods =['POST', 'GET'])
 def index():
-    return 'hello world'
+    return render_template('index.html')
