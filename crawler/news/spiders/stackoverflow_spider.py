165a2285d3a43c283a66d2ec200a9f1bb4393d9b
jinxingh <jinxingh@vmware.com>
2017-02-01 23:18:04 +0800

    Add Spider for stackoverflow.

---
+++ crawler/news/spiders/stackoverflow_spider.py
@@ -0,0 +1,19 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from news.items import BoardNewsItem
+
+class StackOverflowSpider(Spider):
+    name = "stackoverflow"
+    allowed_domains = "stackoverflow.com"
+    start_urls = ["http://stackoverflow.com/?tab=week"]
+
+    def parse(self, response):
+        print "*************************"
+        questions = response.xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = BoardNewsItem()
+            item['title'] = question.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
+            item['url'] = "http://" + self.allowed_domains + question.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+            print item
+            print "*************************"
